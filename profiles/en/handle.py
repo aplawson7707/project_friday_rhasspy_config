ba579c9dc620a7b7552ce7bcb15369ae04c504f5
respeaker <respeaker@seeed.cc>
2022-06-02 20:23:03 -0700

    Update to handle intents via local command

--- profiles/en/handle.py
+++ profiles/en/handle.py
@@ -14,18 +14,34 @@ o = json.loads(sys.stdin.read())
 
 intent = o["intent"]["name"]
 
+greetings = [
+    "Hi",
+    "Hello",
+    "Hey there",
+]
+
+morning_greetings = [
+    "Good morning sir",
+    "Good morning to you as well",
+    "How are you today sir?",
+    "Good to see you sir",
+]
+
 if intent == "GetTime":
     now = datetime.datetime.now()
     speech("It's %s %d %s." % (now.strftime('%H'), now.minute, now.strftime('%p')))
 
 elif intent == "Greeting":
-    replies = [
-        "Hi",
-        "Hello",
-        "Hey there",
-        "How are you",
-    ]
-    speech(random.choice(replies))
+    speech(random.choice(greetings))
+
+elif intent == "GuessWhat":
+    speech("Chicken Butt")
+
+elif intent == "KnockKnock":
+    speech("Nobody's home")
+
+elif intent == "GoodMorning":
+    speech(random.choice(morning_greetings))
 
 # Convert dict to JSON and print to stdout
-erint(json.dumps(o))
+print(json.dumps(o))
